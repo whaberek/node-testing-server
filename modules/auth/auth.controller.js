74f477f96b0233de6323e8a187c95c6ec00b71c3
Wojciech Haberek <wojciech.haberek@idego.pl>
2019-10-17 12:58:11 +0200

    Change error login response status

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -9,7 +9,7 @@ const authController = {
 				token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NzEyNDA0ODUsImV4cCI6MTYwMjc3NjQ4NSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.mozYnG8lPCnQMuZ_9PnySdSSbY5IGKgmO1RBQrHAi9g',
 			});
 		} else {
-			res.json({
+			res.status(401).json({
 				error: 6,
 				message: 'Cannot login user',
 				status: 'UNAUTHORIZED',
